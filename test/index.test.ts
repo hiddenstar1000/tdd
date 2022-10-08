1cd36e178e338d61ecd4dbd9d93209f7500cf43a
Dixon Gunasekara <dixon@randomsoftware.net>
2022-10-08 23:56:49 +0530

    Step1 Red

---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+describe("getMessage()", () => {
+  it("Return + number when it the input is positive", () => {
+    const number = 3;
+    const result = getMessage(number);
+    expect(result).toBe("+ number");
+  });
+});
