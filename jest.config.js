60bcfcbea0dd1aa8758107bc7fd36a265a7fda6b
Dixon Gunasekara <dixon@randomsoftware.net>
2022-10-08 23:19:15 +0530

    Base Project Structure

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  clearMocks: false,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+};
