0a682a0f8e43cbd4439ba11da1ebfe8206bd0851
Dixon Gunasekara <dixon@randomsoftware.net>
2022-10-09 21:11:39 +0530

    Step1 Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export const getMessage = (number: number) => {
-  if (number > 0) return "+ number";
+  if (number > 0) return Message.POSITIVE_NUMBER;
 };
+
+enum Message {
+  POSITIVE_NUMBER = "+ number",
+}
