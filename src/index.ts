be33166bffb2180a8ef93c91bab692d576519a77
Dixon Gunasekara <dixon@randomsoftware.net>
2022-10-09 22:44:21 +0530

    Step2 Green

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export const getMessage = (number: number) => {
   if (number > 0) return Message.POSITIVE_NUMBER;
+  if (number < 0) return Message.NEGATIVE_NUMBER;
 };
 
 enum Message {
   POSITIVE_NUMBER = "+ number",
+  NEGATIVE_NUMBER = "- number",
 }
